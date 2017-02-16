c6327cb7ccdb67ab186603d32f8046c3a19f8d85
Jason Ally <jason.ally@gmail.com>
2017-02-15 21:31:00 -0800

    Add comments

--- tournament.sql
+++ tournament.sql
@@ -10,6 +10,7 @@ DROP DATABASE IF EXISTS tournament;
 
 CREATE DATABASE tournament;
 
+-- PostgreSQL doesn't connect to the database by default
 \c tournament
 
 DROP VIEW IF EXISTS wins;
@@ -24,12 +25,14 @@ CREATE TABLE players(
     name text
 );
 
+-- Winner and loser are foreign keys that reference id in players table
 CREATE TABLE matches(
     id serial PRIMARY KEY,
     winner integer REFERENCES players(id),
     loser integer REFERENCES players(id)
 );
 
+-- COALESCE sets value to 0 if COUNT(m.winner) is null
 CREATE VIEW wins AS
 SELECT p.id AS id,
     COALESCE(COUNT(m.winner), 0) AS matches_won
@@ -48,6 +51,8 @@ ON p.id = m.loser
 GROUP BY p.id
 ORDER BY matches_lost DESC;
 
+-- Joins could be done using WHERE and AND clauses
+-- I find LEFT JOIN, RIGHT JOIN, etc. to be more readable
 CREATE VIEW standings AS
 SELECT p.id AS id,
     p.name AS name,
