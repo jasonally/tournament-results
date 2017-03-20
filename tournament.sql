745c1e66420bef250c54f533d58e83bb2727ac85
Jason Ally <jason.ally@gmail.com>
2017-03-19 18:36:50 -0700

    Refactor to standardize SQL style

--- tournament.sql
+++ tournament.sql
@@ -1,10 +1,4 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
 DROP DATABASE IF EXISTS tournament;
 
@@ -20,33 +14,37 @@ DROP VIEW IF EXISTS standings;
 DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS matches;
 
-CREATE TABLE players(
-    id serial PRIMARY KEY,
-    name text
+CREATE TABLE players
+(
+  id serial PRIMARY KEY,
+  name text
 );
 
 -- Winner and loser are foreign keys that reference id in players table
-CREATE TABLE matches(
-    id serial PRIMARY KEY,
-    winner integer REFERENCES players(id),
-    loser integer REFERENCES players(id)
+CREATE TABLE matches
+(
+  id serial PRIMARY KEY,
+  winner integer REFERENCES players(id),
+  loser integer REFERENCES players(id)
 );
 
 -- COALESCE sets value to 0 if COUNT(m.winner) is null
 CREATE VIEW wins AS
-SELECT p.id AS id,
-    COALESCE(COUNT(m.winner), 0) AS matches_won
-FROM players p
-LEFT JOIN matches m
+SELECT
+  p.id AS id,
+  COALESCE(COUNT(m.winner), 0) AS matches_won
+FROM players AS p
+LEFT JOIN matches AS m
 ON p.id = m.winner
 GROUP BY p.id
 ORDER BY matches_won DESC;
 
 CREATE VIEW losses AS
-SELECT p.id AS id,
-    COALESCE(COUNT(m.loser), 0) AS matches_lost
-FROM players p
-LEFT JOIN matches m
+SELECT
+  p.id AS id,
+  COALESCE(COUNT(m.loser), 0) AS matches_lost
+FROM players AS p
+LEFT JOIN matches AS m
 ON p.id = m.loser
 GROUP BY p.id
 ORDER BY matches_lost DESC;
@@ -54,13 +52,14 @@ ORDER BY matches_lost DESC;
 -- Joins could be done using WHERE and AND clauses
 -- I find LEFT JOIN, RIGHT JOIN, etc. to be more readable
 CREATE VIEW standings AS
-SELECT p.id AS id,
-    p.name AS name,
-    w.matches_won as wins,
-    (l.matches_lost + w.matches_won) AS matches
-FROM players p
-LEFT JOIN wins w
+SELECT
+  p.id AS id,
+  p.name AS name,
+  w.matches_won as wins,
+  (l.matches_lost + w.matches_won) AS matches
+FROM players AS p
+LEFT JOIN wins AS w
 ON p.id = w.id
-LEFT JOIN losses l
+LEFT JOIN losses AS l
 ON p.id = l.id
 ORDER BY wins DESC;
